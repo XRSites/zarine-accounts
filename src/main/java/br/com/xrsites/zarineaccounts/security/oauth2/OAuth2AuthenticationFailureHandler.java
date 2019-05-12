fc84e7be34f0b30f0cae7ca89c0346165fffa0c8
Guilherme Luiz Argentino Silva <info@xrs.com.br>
2019-05-11 23:44:21 -0300

    Debugging

--- src/main/java/br/com/xrsites/zarineaccounts/security/oauth2/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/br/com/xrsites/zarineaccounts/security/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -1,6 +1,8 @@
 package br.com.xrsites.zarineaccounts.security.oauth2;
 
 import br.com.xrsites.zarineaccounts.util.CookieUtils;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -16,6 +18,7 @@ import java.io.IOException;
 import static br.com.xrsites.zarineaccounts.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
 @Component
+@Slf4j
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Autowired
@@ -23,6 +26,9 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+    	
+    	log.debug("Login unsucessful: {0}", exception);
+    	
         String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
                 .map(Cookie::getValue)
                 .orElse(("/"));
