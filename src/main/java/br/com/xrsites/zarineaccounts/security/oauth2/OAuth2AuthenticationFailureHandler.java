a123b81a3c4c558e14f6d47853d137a25cdf1d7f
Guilherme Luiz Argentino Silva <info@xrs.com.br>
2019-05-11 23:57:28 -0300

    Debugging

--- src/main/java/br/com/xrsites/zarineaccounts/security/oauth2/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/br/com/xrsites/zarineaccounts/security/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -27,7 +27,7 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
     	
-    	log.debug("Login unsucessful: {0}", exception);
+    	log.debug("Login unsucessful", request.getRequestURL(), response.getStatus(), exception);
     	
         String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
                 .map(Cookie::getValue)
